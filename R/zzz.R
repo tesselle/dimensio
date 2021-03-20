f9e8256b7ab93d73f3f2678c9efe0519a42e2dd5
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-03-20 09:20:48 +0100

    Use onLoad for package options

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
-.onAttach <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) {
   op <- options()
-  op.arkhe <- list(
+  op.dimensio <- list(
     dimensio.verbose = TRUE,
     dimensio.digits = 3,
     dimensio.max.print = 10
   )
-  toset <- !(names(op.arkhe) %in% names(op))
-  if(any(toset)) options(op.arkhe[toset])
+  toset <- !(names(op.dimensio) %in% names(op))
+  if(any(toset)) options(op.dimensio[toset])
 
   invisible()
 }
