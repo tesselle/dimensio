162a565729d2759920a4a8b70793ce03e185be5c
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2024-08-24 15:48:54 +0200

    Run some tests only on Linux

--- inst/tinytest/test_bootstrap_ca.R
+++ inst/tinytest/test_bootstrap_ca.R
@@ -1,11 +1,10 @@
-if (at_home()) {
+if (at_home() && Sys.info()["sysname"] == "Linux") {
   source("helpers.R")
   using("tinysnapshot")
   options(tinysnapshot_device = "svglite")
   options(tinysnapshot_height = 7) # inches
   options(tinysnapshot_width = 7)
   options(tinysnapshot_tol = 200) # pixels
-  options(tinysnapshot_os = "Linux")
 
   data("colours")
 
