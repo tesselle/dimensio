d4c0df57c2d07517c6f6b27a42aa4faf36d004a2
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2024-08-25 19:21:21 +0200

    Reorganize code

--- R/mutators.R
+++ R/mutators.R
@@ -3,6 +3,20 @@
 NULL
 
 # Non exported =================================================================
+is_centered <- function(x) {
+  !all(x@center == 0)
+}
+is_scaled <- function(x) {
+  !all(x@scale == 1)
+}
+
+has_supplementary <- function(x, margin = 1) {
+  margin <- margin[[1L]]
+  if (margin == 1) supp <- any(x@rows@supplement)
+  if (margin == 2) supp <- any(x@columns@supplement)
+  supp
+}
+
 get_masses <- function(x, margin = 1) {
   margin <- margin[[1L]]
   if (margin == 1) mass <- x@rows@weights
