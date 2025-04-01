19ab93ba713e2c71a6aa68ec3c8923e88d53de85
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2025-04-01 13:40:48 +0200

    Increase tests tolerance

--- inst/tinytest/test_plot_pca.R
+++ inst/tinytest/test_plot_pca.R
@@ -5,7 +5,7 @@ if (at_home()) {
   options(tinysnapshot_device = "svglite")
   options(tinysnapshot_height = 7) # inches
   options(tinysnapshot_width = 7)
-  options(tinysnapshot_tol = 200) # pixels
+  options(tinysnapshot_tol = 300) # pixels
   options(tinysnapshot_os = "Linux")
 
   data("iris")
